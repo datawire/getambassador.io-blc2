9ec5f9e989ed21e61b09b05c594abc2a05405934
Luke Shumaker <lukeshu@datawire.io>
2021-06-21 15:09:44 -0600

    Fix some things found by mypy --strict
    The 'requests', 'urllib3', and 'bs4' stubs are still too incomplete to
    turn on --strict for real, but trying to run it did find a few things.

--- blclib/__init__.py
+++ blclib/__init__.py
@@ -1,2 +1,9 @@
 from .checker import BaseChecker, get_content_type
 from .models import Link, URLReference
+
+__all__ = [
+    'BaseChecker',
+    'get_content_type',
+    'Link',
+    'URLReference',
+]
