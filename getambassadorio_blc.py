52b1ff00392d8ab8e37b93cb2933e961d41d0db1
Arturo Gonzalez <arturogonzalez@datawire.io>
2021-11-03 19:40:55 -0600

    this fix would be stop detecting sanity images with a rect attribute as broken links.

--- getambassadorio_blc.py
+++ getambassadorio_blc.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
+import os
 import re
+import subprocess
 import sys
-from typing import Optional
-from urllib.parse import urlparse
+import threading
+from typing import Optional, List
+from urllib.parse import urlparse, urldefrag
 
 from blclib import Link, URLReference
-from generic_blc import GenericChecker, main
+from generic_blc import GenericChecker, CheckerInterface
 
 
 def is_doc_url(url: URLReference) -> Optional[str]:
@@ -24,6 +27,21 @@ def urlpath(url: str) -> str:
 
 
 class AmbassadorChecker(GenericChecker):
+    stats_broken_links: int = 0
+    stats_ugly_links: int = 0
+
+    def log_broken(self, link: Link, reason: str) -> None:
+        self.stats_broken_links += 1
+        msg = f'Page {urldefrag(link.pageurl.resolved).url} has a broken link: "{link.linkurl.ref}" ({reason})'
+        print(msg)
+
+    def log_ugly(self, link: Link, reason: str, suggestion: Optional[str] = None) -> None:
+        self.stats_ugly_links += 1
+        msg = f'Page {urldefrag(link.pageurl.resolved).url} has an ugly link: "{link.linkurl.ref}" {reason}'
+        if suggestion:
+            msg += f' (did you mean "{suggestion}"?)'
+        print(msg)
+
     def is_internal_domain(self, netloc: str) -> bool:
         if netloc == 'blog.getambassador.io':
             return False
@@ -39,17 +57,20 @@ class AmbassadorChecker(GenericChecker):
 
     def product_should_skip_link(self, link: Link) -> bool:
         links_to_skip = [
-            'http://verylargejavaservice:8080',
+            'http://verylargejavaservice:8080/',
             'https://blog.getambassador.io/search?q=canary',
-            'https://app.datadoghq.com/apm/traces',
+            'https://app.datadoghq.com/apm/traces/',
             'http://web-app.emojivoto/',
-            'http://web-app.emojivoto/leaderboard',
+            'http://web-app.emojivoto/leaderboard/',
             'http://verylargejavaservice.default:8080/color',
             'http://localhost:8080/',
             'http://localhost:8083/',
+            'http://localhost:8083/leaderboard/',
             'http://verylargejavaservice.default:8080/',
         ]
-        return link.linkurl.ref in links_to_skip or 'mailto' in link.linkurl.ref
+        return len(
+            [True for link_to_skip in links_to_skip if link.linkurl.ref in link_to_skip]
+        ) > 0 or 'mailto' in link.linkurl.ref
 
     def product_should_skip_link_result(self, link: Link, broken: str) -> bool:
         return bool(
@@ -87,8 +108,8 @@ class AmbassadorChecker(GenericChecker):
                     link=link,
                     reason='is a canonical but does not point at www.getambassador.io',
                     suggestion=urlparse(link.linkurl.resolved)
-                    ._replace(scheme='https', netloc='www.getambassador.io')
-                    .geturl(),
+                        ._replace(scheme='https', netloc='www.getambassador.io')
+                        .geturl(),
                 )
             # Other than that, the canonicals don't need to be inspected more, because they're
             # allowed (expected!) to be cross-version.
@@ -111,6 +132,61 @@ class AmbassadorChecker(GenericChecker):
                     link=link,
                     reason=f'is a link from docs version={src_ver} to docs version={dst_ver}',
                 )
+
+    @staticmethod
+    def _parse_srcset_value(attrvalue: str) -> List:
+        delimiter = ','
+        if "&rect=" in attrvalue and "https://cdn.sanity.io/images/" in attrvalue:
+            links = []
+            tokens = attrvalue.split(',', 4)
+            while len(tokens) >= 4:
+                links.append(delimiter.join(tokens[0:4]).split(' ')[0])
+                attrvalue = attrvalue[attrvalue.find('h', len(delimiter.join(tokens[0:4]))):]
+                tokens = attrvalue.split(delimiter, 5)
+            return links
+        else:
+            return [desc.split()[0] for desc in attrvalue.split(',')]
+
+
+def main(checkerCls: CheckerInterface, projdir: str) -> int:
+    urls = [
+        'http://localhost:9000/summer-of-k8s/code/',
+    ]
+    checker = checkerCls(domain=urlparse(urls[0]).netloc)
+    for url in urls:
+        checker.enqueue(URLReference(ref=url))
+
+    with subprocess.Popen(
+        [os.path.join(os.path.dirname(__file__), 'serve.js')],
+        cwd=projdir,
+        stdout=subprocess.PIPE,
+    ) as srv:
+        try:
+            # Wait for the server to be ready
+            assert srv.stdout
+            sys.stdout.write(srv.stdout.readline().decode('utf-8'))
+
+            # Pump the servers logs
+            def pump():
+                while line := srv.stdout.readline():
+                    sys.stdout.write(line.decode('utf-8'))
+
+            threading.Thread(target=pump).start()
+
+            # Run the checker
+            checker.run()
+        finally:
+            srv.kill()
+
+    # Print a summary
+    print("Summary:")
+    print(
+        f"  Actions: Sent {checker.stats_requests} HTTP requests and slept for {checker.stats_sleep} seconds in order to check {checker.stats_links_total} links on {checker.stats_pages} pages."
+    )
+    print(
+        f"  Results: Encountered {checker.stats_ugly_links + checker.stats_broken_links} errors, {checker.stats_links_bad} bad links."
+    )
+    return 1 if checker.stats_broken_links > 0 else 0
 
 
 if __name__ == "__main__":
