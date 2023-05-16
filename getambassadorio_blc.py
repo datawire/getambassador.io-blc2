9124050734fe19acfb8354052cc811894b06e4ce
joseocampo-gomez@datawire.io <joseocampo-gomez@datawire.io>
2023-05-15 19:16:58 -0600

    add twitter userAgent

--- getambassadorio_blc.py
+++ getambassadorio_blc.py
@@ -80,6 +80,7 @@ class AmbassadorChecker(GenericChecker):
         "tanzu.vmware.com": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36",
         "help.github.com": "Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0",
         "docs.github.com": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36",
+        "twitter.com": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36",
         "comparably.com": "Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0",
     }
 
