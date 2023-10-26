df65a94f855066b13f9fa8ef197cf81aa6ffb72e
Andres Osorio <aosorio@datawire.io>
2023-10-26 11:52:47 -0400

    Fix stub

--- mypy-stubs/webencodings/__init__.py
+++ mypy-stubs/webencodings/__init__.py
@@ -6,7 +6,7 @@ class Encoding(object):
     codec_info: codecs.CodecInfo
 
     def __init__(self, name: str, codec_info: codecs.CodecInfo) -> None:
-        ...
+        self.name = name
 
     def __repr__(self) -> str:
-        ...
+        return '<Encoding %s>' % self.name
